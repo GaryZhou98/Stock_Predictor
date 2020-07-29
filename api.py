935c0da21219291eaac4b51e0eb7503c78f9d042
esaltzm1 <edward_saltzman@brown.edu>
2020-07-29 14:50:33 -0400

    factored out some code

--- api.py
+++ api.py
@@ -1,66 +1,28 @@
 # from iexfinance.stocks import get_historical_data
-from datetime import datetime, timedelta
-import csv
 import argparse
 import csv
 import datetime
 import json
-import pandas as pd
-import numpy as np
-import requests
 import time
+from datetime import datetime, timedelta
 
-COVID_CSV_PATH = "https://covidtracking.com/api/v1/us/daily.csv"
-COVID_COLUMNS = ['date','positive','negative','death','hospitalized','deathIncrease','hospitalizedIncrease','negativeIncrease', 'positiveIncrease']
-RECOMMENDATION_TRENDS_PATH = (
-    "https://finnhub.io/api/v1/stock/recommendation?symbol={}&token={}"
-)
-NEWS_SENTIMENT_PATH = "https://finnhub.io/api/v1/news-sentiment?symbol={}&token={}"
-DAILY_PRICES_PATH = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=full&apikey={}"
+import numpy as np
+import pandas as pd
+import requests
 
-PRICE_INDICATORS = {
-    "ATR": "https://www.alphavantage.co/query?function=ATR&symbol={}&interval=daily&time_period=14&apikey={}",
-    "SMA": "https://www.alphavantage.co/query?function=SMA&symbol={}&interval=daily&time_period=10&series_type=close&apikey={}",
-    "EMA": "https://www.alphavantage.co/query?function=EMA&symbol={}&interval=daily&time_period=10&series_type=close&apikey={}",
-    "MACD": "https://www.alphavantage.co/query?function=MACD&symbol={}&interval=daily&series_type=close&apikey={}",
-    "STOCH": "https://www.alphavantage.co/query?function=STOCH&symbol={}&interval=daily&apikey={}",
-    "RSI": "https://www.alphavantage.co/query?function=RSI&symbol={}&interval=daily&time_period=10&series_type=close&apikey={}",
-    "ADX": "https://www.alphavantage.co/query?function=ADX&symbol={}&interval=daily&time_period=10&apikey={}",
-    "BBANDS": "https://www.alphavantage.co/query?function=BBANDS&symbol={}&interval=daily&time_period=5&series_type=close&nbdevup=3&nbdevdn=3&apikey={}",
-    "OBV": "https://www.alphavantage.co/query?function=OBV&symbol=IBM&interval=daily&apikey={}",
-    "CCI": "https://www.alphavantage.co/query?function=CCI&symbol={}&interval=daily&time_period=10&apikey={}",
-    "AROON": "https://www.alphavantage.co/query?function=AROON&symbol={}&interval=daily&time_period=14&apikey={}",
-}
+import constants as c
 
-
-UNEMPLOYMENT_SERIES_ID = "LNS14000000"  # seasonally adj. from BLS website
-BLS_API_URL = "https://api.bls.gov/publicAPI/v2/timeseries/data/"
-
-
-TIME_FORMAT = "%Y-%m-%d"
-SAVED_CSV_PATH = "{}_daily.csv"
-COVID_DATA_PATH = "covid.csv"
-recommendation_cols = ["date", "buy", "hold", "sell"]
-credentials = json.load(open("credentials.json", "r"))
-end = datetime.datetime.now()
-
-# pe = fb.get_earnings(period='year', token="pk_69c9cac10e344939be9ee5694af27d49")[0]['actualEPS']
-eps = 7.30  # TTM
+num_av_requests = 0
+credentials = json.load(open(c.CREDENTIALS, "r"))
+end = datetime.now()
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-d",
-        "--start_date",
-        help="start date TIME_FORMAT",
-        type=lambda s: datetime.datetime.strptime(s, TIME_FORMAT),
-        default=(datetime.date.today() - timedelta(1)).strftime(TIME_FORMAT),
-    )
     parser.add_argument("-s", "--symbol", help="symbol to pull data on", default=False)
     parser.add_argument("-f", "--file", help="pull symbols from file", default=False)
     parser.add_argument(
-        "-n", "--new_row", help="add new data to existing csv", action="store_false"
+        "-c", "--covid_only", help="only pull covid data", action="store_true"
     )
     return parser.parse_args()
 
@@ -71,94 +33,49 @@ def get_row_from_csv(csv_fname):
             yield row
 
 
-def get_new_row(symbol, file):
-    prev_price_data = pd.read_csv(file).tail(4)
-    last_date = prev_price_data.tail(1)["date"].values[0]
-    if last_date >= (datetime.date.today() - timedelta(1)).strftime(TIME_FORMAT):
-        print(
-            f"Newest data for {symbol} has already been appended, aborting operation."
-        )
-        return
-    prev_price_data = prev_price_data["closePrice"].to_numpy().astype("float64")
-    price = requests.get(PRICE_PATH.format(symbol, credentials["av_api_key"])).json()
-    price = float(
-        price["Time Series (Daily)"][
-            (datetime.date.today() - timedelta(1)).strftime(TIME_FORMAT)
-        ]["4. close"]
-    )
-    pe = price / eps
-    simple_avg = 0
-    total = 0
-    for num in prev_price_data:
-        total += float(num)
-    simple_avg = (total + price) / 5
-    price_data = {
-        "date": [datetime.date.today() - timedelta(1)],
-        "closePrice": [price],
-        "simpleAvg": [simple_avg],
-        "pe": [pe],
-    }
-    price_data = pd.DataFrame.from_dict(price_data)
-    covid_data = get_covid_data(
-        (datetime.date.today() - timedelta(1)).strftime(TIME_FORMAT)
-    )
-    recommendation_trends = requests.get(
-        RECOMMENDATION_TRENDS_PATH.format(symbol, credentials["finnhub_api_key"])
-    ).json()[0]
-    recommendation_trends = pd.DataFrame(recommendation_trends, index=[0]).drop(
-        ["period", "symbol"], axis=1
-    )
-    atr = get_atr(
-        symbol, (datetime.date.today() - timedelta(1)).strftime(TIME_FORMAT)
-    ).reset_index()
-    all_data = price_data.merge(
-        covid_data, how="inner", left_index=True, right_index=True
-    )
-    all_data = all_data.merge(
-        recommendation_trends, how="outer", left_index=True, right_index=True
-    )
-    all_data = all_data.merge(atr, how="outer", left_index=True, right_index=True)
-    all_data = all_data.fillna(method="backfill")
-    all_data = all_data.drop(["date_y", "index", "date"], axis=1).rename(
-        columns={"date_x": "date"}
-    )
-    all_data.to_csv(SAVED_CSV_PATH.format(symbol), index=False, mode="a", header=False)
-    print(f"added new {symbol} data to {SAVED_CSV_PATH.format(symbol)}.")
+def make_av_request(path):
+    global num_av_requests
+    if num_av_requests % 5 == 0:
+        print("maximum API requests per minute reached. waiting one minute...")
+        time.sleep(60)
+    num_av_requests += 1
+    return requests.get(path).json()
 
 
-def get_data(symbol, start_date):
+def get_price_data(symbol):
     print(f"pulling historical data for {symbol}...")
     daily_prices = get_daily_prices(symbol)
     price_indicators = get_price_indicators(symbol)
     historical = daily_prices.merge(price_indicators, how="inner", on="date")
     historical.insert(1, "close", historical.pop("close"))
-    historical.to_csv(SAVED_CSV_PATH.format(symbol), index=False)
-    print(f"wrote {symbol} data to {SAVED_CSV_PATH.format(symbol)}.")
-    covid_data = get_covid_data()
-    covid_data.to_csv(COVID_DATA_PATH, index=False)
-    print(f"wrote covid data data to {COVID_DATA_PATH}.")
+    historical.to_csv(c.SAVED_CSV_PATH.format(symbol), index=False)
+    print(f"wrote {symbol} data to {c.SAVED_CSV_PATH.format(symbol)}.")
 
 
 def get_covid_data():
-    data = pd.read_csv(COVID_CSV_PATH)
-    data["date"] = pd.to_datetime(data["date"], format='%Y%m%d')
-    data = data.sort_values(by='date', ascending=True)
+    data = pd.read_csv(c.COVID_CSV_PATH)
+    data["date"] = pd.to_datetime(data["date"], format="%Y%m%d")
+    data = data.sort_values(by="date", ascending=True)
     data.reset_index(inplace=True)
-    data = data[COVID_COLUMNS]
+    data = data[c.COVID_COLUMNS]
     data.fillna(0, inplace=True)
-    return data
+    data.to_csv(c.COVID_DATA_PATH, index=False)
+    print(f"wrote covid data data to {c.COVID_DATA_PATH}.")
 
 
 def get_daily_prices(symbol):
-    daily_prices = requests.get(
-        DAILY_PRICES_PATH.format(symbol, credentials["av_api_key"])
-    ).json()
+    daily_prices = make_av_request(
+        c.DAILY_PRICES_PATH.format(symbol, credentials["av_api_key"])
+    )
+    import ipdb
+
+    ipdb.set_trace()
     daily_prices = pd.DataFrame.from_dict(
         daily_prices["Time Series (Daily)"], orient="index"
     )
     daily_prices.drop(columns=["5. volume"], inplace=True)
     daily_prices.reset_index(inplace=True)
-    daily_prices["index"] = pd.to_datetime(daily_prices["index"], format=TIME_FORMAT)
+    daily_prices["index"] = pd.to_datetime(daily_prices["index"], format=c.TIME_FORMAT)
     daily_prices.rename(
         columns={
             "index": "date",
@@ -174,32 +91,22 @@ def get_daily_prices(symbol):
 
 
 def get_price_indicators(symbol):
-    print("maximum API requests per minute reached. waiting one minute...")
-    time.sleep(60)
     indicators = pd.DataFrame(columns=["date"])
-    i = 0
-    for indicator, path in PRICE_INDICATORS.items():
-        if i % 5 == 0:
-            print(indicator)
-            print("maximum API requests per minute reached. waiting one minute...")
-            time.sleep(60)
+    for indicator, path in c.PRICE_INDICATORS.items():
         indicators = indicators.merge(
             get_price_indicator(symbol, indicator, path), how="outer", on="date"
         )
-        i += 1
-    print("maximum API requests per minute reached. waiting one minute...")
-    time.sleep(60)
     return indicators.dropna()
 
 
 def get_price_indicator(symbol, indicator, path):
     path = path.format(symbol, credentials["av_api_key"])
-    p_i = requests.get(path).json()
+    p_i = make_av_request(path)
     p_i = pd.DataFrame.from_dict(
         p_i[f"Technical Analysis: {indicator}"], orient="index"
     )
     p_i.reset_index(inplace=True)
-    p_i["index"] = pd.to_datetime(p_i["index"], format=TIME_FORMAT)
+    p_i["index"] = pd.to_datetime(p_i["index"], format=c.TIME_FORMAT)
     p_i.rename(columns={"index": "date", indicator: indicator.lower()}, inplace=True)
     return p_i
 
@@ -208,12 +115,12 @@ def get_unemployment(start_date):
     headers = {"Content-type": "application/json"}
     data = json.dumps(
         {
-            "seriesid": [UNEMPLOYMENT_SERIES_ID],
+            "seriesid": [c.UNEMPLOYMENT_SERIES_ID],
             "startyear": start_date.year,
             "endyear": datetime.date.today().year,
         }
     )
-    res = json.loads(requests.post(BLS_API_URL, data=data, headers=headers).text)
+    res = json.loads(requests.post(c.BLS_API_URL, data=data, headers=headers).text)
     df = pd.DataFrame.from_dict(res["Results"]["series"][0]["data"])
     df["period"] = pd.to_datetime(df["periodName"] + " " + df["year"])
     df.drop(columns=["year", "periodName", "latest", "footnotes"], inplace=True)
@@ -227,7 +134,7 @@ def get_unemployment(start_date):
 
 def get_recommendation_trends(symbol, start_date):
     all_recs = requests.get(
-        RECOMMENDATION_TRENDS_PATH.format(symbol, credentials["finnhub_api_key"])
+        c.RECOMMENDATION_TRENDS_PATH.format(symbol, credentials["finnhub_api_key"])
     ).json()
     all_recs = pd.DataFrame.from_records(all_recs)
     first_day_of_month = start_date.replace(day=1)
@@ -239,33 +146,26 @@ def get_recommendation_trends(symbol, start_date):
     assert (
         all_recs.size >= total_expected_recs
     ), f"{symbol} does not have enough recommendation trends for requested time period."
-    first_day_of_month = first_day_of_month.strftime(TIME_FORMAT)
+    first_day_of_month = first_day_of_month.strftime(c.TIME_FORMAT)
     all_recs = all_recs[all_recs["period"] >= first_day_of_month]
     index = pd.date_range(all_recs["period"].min(), datetime.date.today())
-    all_recs["date"] = pd.to_datetime(all_recs["period"], format=TIME_FORMAT)
+    all_recs["date"] = pd.to_datetime(all_recs["period"], format=c.TIME_FORMAT)
     all_recs = all_recs.set_index("date").reindex(index, method="backfill")
     all_recs = all_recs.reset_index()
     all_recs["date"] = all_recs["index"]
-    return all_recs[all_recs["date"] >= start_date][recommendation_cols]
-
-
-def get_news_sentimenet(symbol, start_date):
-    print("get_news_sentiment is NOT YET IMPLEMENTED")
+    return all_recs[all_recs["date"] >= start_date][c.RECOMMENDATION_COLUMNS]
 
 
 if __name__ == "__main__":
     args = get_args()
-    if args.new_row:
-        if not args.symbol or not args.file:
-            raise ValueError(
-                "Missing start_date, symbol and filepath for existing csv to add new row"
-            )
-        get_new_row(args.symbol, args.file)
-        exit(1)
+    if args.covid_only:
+        get_covid_data()
     elif args.symbol:
-        get_data(args.symbol, args.start_date)
-        if args.file:
-            symbols = iter(get_row_from_csv(args.file))
-            for row in symbols:
-                for symbol in row:
-                    get_data(symbol, args.start_date)
+        get_covid_data()
+        get_price_data(args.symbol)
+    elif args.file:
+        get_covid_data()
+        symbols = iter(get_row_from_csv(args.file))
+        for row in symbols:
+            for symbol in row:
+                get_price_data(symbol.strip())
--- model.py
+++ model.py
@@ -1,181 +1,107 @@
 import argparse
-import pandas as pd
 
-
-from tensorflow.keras import Sequential
-from tensorflow.keras.models import Model
-from tensorflow.keras import layers, optimizers
-from tensorflow.keras.layers import Dense, Input, Activation, LSTM, Dropout, BatchNormalization, concatenate
-from matplotlib import pyplot
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from api import COVID_COLUMNS
 import numpy as np
-import ipdb
+import pandas as pd
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras import Sequential, layers, optimizers
+from tensorflow.keras.layers import (
+    LSTM,
+    Activation,
+    BatchNormalization,
+    Dense,
+    Dropout,
+    Input,
+    concatenate,
+)
+from tensorflow.keras.models import Model, load_model
 
-# preprocessing constants
-TIME_STEP = 5
-PREDICTION_STEP = 1
-
-# model constants
-LSTM_OUTPUT_SIZE = 128
-DENSE1_OUTPUT_SIZE = 64
-DENSE2_OUTPUT_SIZE = 32
-DENSE3_OUTPUT_SIZE = PREDICTION_STEP
+import constants as c
+from preprocessing import prepare_data, prepare_prediction_data
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-s", "--symbol", help="symbol to pull data on", default=False)
+    parser.add_argument("-s", "--symbol", help="symbol to pull data on", default=False)
     return parser.parse_args()
 
 
-def series_to_sl(values, is_covid=False):
-    # values = scaler.transform(values)
-
-    # Split data into input and labels
-    input_data = np.array(values[0: len(values)-PREDICTION_STEP])
-    label = np.array(values[TIME_STEP:, 0]).reshape(-1, 1)
-
-    # Normalization of data
-    for i in range(0, input_data.shape[1]):
-        temp = input_data[:, i].reshape(-1, 1)
-        input_data[:, i] = MinMaxScaler(
-            feature_range=(0, 1)).fit(temp).transform(temp)[:, 0]
-
-    # x_scaler = MinMaxScaler(feature_range=(0, 1)).fit(input_data)
-    # input_data = x_scaler.transform(input_data)
-    y_scaler = MinMaxScaler(feature_range=(0, 1)).fit(label)
-    label = y_scaler.transform(label)
-
-    x = []
-    y = []
-
-    # reshape input and label
-
-    for start, end in zip(range(0, len(input_data) - TIME_STEP), range(TIME_STEP, len(input_data))):
-        if is_covid:
-            x.append(input_data[start:end, 1:])
-        else:
-            x.append(input_data[start:end])
-    for start, end in zip(range(0, len(label) - PREDICTION_STEP), range(PREDICTION_STEP, len(label))):
-        y.append(label[start:end, 0])
-
-    x = np.array(x)
-    y = np.array(y)
-    # ipdb.set_trace()
-    return x, y, y_scaler
-
-
-def prepare_data(filepath, shuffle=True):
-
-    price_df = pd.read_csv(filepath, header=0)
-    covid_df = pd.read_csv('covid.csv', header=0)
-    covid_df = price_df.merge(covid_df, how='left', on='date')
-    covid_df.fillna(value=0, inplace=True)
-    covid_df = covid_df[['close']+COVID_COLUMNS]
-    covid_df.drop(['date'], axis=1, inplace=True)
-    price_df.drop(['date'], axis=1, inplace=True)
-    price_values = price_df.to_numpy().astype("float32")
-    covid_values = covid_df.to_numpy().astype("float32")
-
-    covid_input, covid_label, scaler = series_to_sl(
-        covid_values, is_covid=True)
-
-    test_portion = int(covid_label.shape[0]*0.15)
-    overall_portion = int(covid_label.shape[0]*0.25)
-    x_train, x_test, y_train, y_test = train_test_split(
-        covid_input, covid_label, test_size=test_portion, shuffle=shuffle)
-
-    # Further split training data to train for individual model vs. train for overall model
-
-    x_train_overall = x_train[len(x_train)-overall_portion:, :, :]
-    y_train_overall = y_train[len(y_train)-overall_portion:, :]
-    x_train_individual = x_train[:len(x_train)-overall_portion, :, :]
-    y_train_individual = y_train[:len(y_train)-overall_portion, :]
-
-    covid_data = {"x_train_individual": x_train_individual,
-                  "x_test": x_test,
-                  "y_train_individual": y_train_individual,
-                  "y_test": y_test,
-                  "x_train_overall": x_train_overall,
-                  "y_train_overall": y_train_overall,
-                  "scaler": scaler}
-
-    input_data, label, scaler = series_to_sl(price_values, is_covid=False)
-    x_train, x_test, y_train, y_test = train_test_split(
-        input_data, label, test_size=test_portion, shuffle=shuffle)
-
-    x_train_overall = x_train[len(x_train)-overall_portion:, :, :]
-    y_train_overall = y_train[len(y_train)-overall_portion:, :]
-    x_train_individual = x_train[:len(x_train)-overall_portion, :, :]
-    y_train_individual = y_train[:len(y_train)-overall_portion, :]
-
-    price_data = {"x_train_individual": x_train_individual,
-                  "x_test": x_test,
-                  "y_train_individual": y_train_individual,
-                  "y_test": y_test,
-                  "x_train_overall": x_train_overall,
-                  "y_train_overall": y_train_overall,
-                  "scaler": scaler}
-
-    return price_data, covid_data
-
-
-def build_covid_model(x_train, y_train, batch_size, epochs,):
+def build_covid_model(x_train, y_train, batch_size):
     covid_model = Sequential()
     covid_data = x_train
-    covid_model.add(LSTM(LSTM_OUTPUT_SIZE, input_shape=(
-        covid_data.shape[1:]), return_sequences=True))
+    covid_model.add(
+        LSTM(
+            c.LSTM_OUTPUT_SIZE,
+            input_shape=(covid_data.shape[1:]),
+            return_sequences=True,
+        )
+    )
     covid_model.add(Dropout(0.2))
     covid_model.add(BatchNormalization())
 
-    covid_model.add(LSTM(LSTM_OUTPUT_SIZE, return_sequences=False))
+    covid_model.add(LSTM(c.LSTM_OUTPUT_SIZE, return_sequences=False))
     covid_model.add(Dropout(0.2))
     covid_model.add(BatchNormalization())
 
-    covid_model.add(Dense(DENSE2_OUTPUT_SIZE, activation='relu'))
-    covid_model.add(Dense(DENSE3_OUTPUT_SIZE, activation='sigmoid'))
+    covid_model.add(Dense(c.DENSE1_OUTPUT_SIZE, activation="relu"))
+    covid_model.add(Dense(c.DENSE2_OUTPUT_SIZE, activation="relu"))
+    covid_model.add(Dense(c.DENSE3_OUTPUT_SIZE, activation="sigmoid"))
 
-    covid_model.compile(loss='mse', optimizer='adam')
+    covid_model.compile(loss="mse", optimizer="adam")
 
-    covid_model.fit(covid_data, y_train, batch_size=batch_size,
-                    epochs=epochs, shuffle=True)
+    covid_model.fit(
+        covid_data,
+        y_train,
+        batch_size=batch_size,
+        epochs=c.COVID_EPOCH,
+        shuffle=c.TRAIN_SHUFFLE,
+    )
     return covid_model
 
 
-def build_price_model(x_train, y_train, batch_size, epochs,):
+def build_price_model(x_train, y_train, batch_size):
     price_model = Sequential()
     price_data = x_train
 
-    price_model.add(LSTM(LSTM_OUTPUT_SIZE, input_shape=(
-        price_data.shape[1:]), return_sequences=False))
+    price_model.add(
+        LSTM(
+            c.LSTM_OUTPUT_SIZE,
+            input_shape=(price_data.shape[1:]),
+            return_sequences=False,
+        )
+    )
     price_model.add(Dropout(0.2))
     price_model.add(BatchNormalization())
 
-    # price_model.add(LSTM(LSTM_OUTPUT_SIZE,return_sequences=False))
-    # price_model.add(Dropout(0.2))
-    # price_model.add(BatchNormalization())
+    price_model.add(Dense(c.DENSE1_OUTPUT_SIZE, activation="relu"))
+    price_model.add(Dense(c.DENSE2_OUTPUT_SIZE, activation="relu"))
+    price_model.add(Dense(c.DENSE3_OUTPUT_SIZE, activation="sigmoid"))
 
-    price_model.add(Dense(DENSE2_OUTPUT_SIZE, activation='relu'))
-    price_model.add(Dense(DENSE3_OUTPUT_SIZE, activation='sigmoid'))
+    price_model.compile(loss="mse", optimizer="adam")
 
-    price_model.compile(loss='mse', optimizer='adam')
-
-    price_model.fit(price_data, y_train, batch_size=batch_size,
-                    epochs=epochs, shuffle=True)
+    price_model.fit(
+        price_data,
+        y_train,
+        batch_size=batch_size,
+        epochs=c.PRICE_EPOCH,
+        shuffle=c.TRAIN_SHUFFLE,
+    )
     return price_model
 
 
-def build_overall_model(price_data, covid_data, batch_size, epochs,):
-    # covid_model
-
+def build_overall_model(price_data, covid_data, batch_size):
     input_models = [
-        build_covid_model(covid_data['x_train_individual'],
-                          covid_data['y_train_individual'], batch_size, epochs//100),
-        build_price_model(price_data['x_train_individual'],
-                          price_data['y_train_individual'], batch_size, epochs)
+        build_covid_model(
+            covid_data["x_train_individual"],
+            covid_data["y_train_individual"],
+            batch_size,
+        ),
+        build_price_model(
+            price_data["x_train_individual"],
+            price_data["y_train_individual"],
+            batch_size,
+        ),
     ]
     for i in range(len(input_models)):
         model = input_models[i]
@@ -183,79 +109,85 @@ def build_overall_model(price_data, covid_data, batch_size, epochs,):
             # make not trainable
             layer.trainable = False
             # rename to avoid 'unique layer name' issue
-            layer._name = 'ensemble_' + str(i+1) + '_' + layer.name
-
+            layer._name = "ensemble_" + str(i + 1) + "_" + layer.name
 
     # define multi-headed input
     ensemble_inputs = [model.input for model in input_models]
-    # ipdb.set_trace()
-	# concatenate merge output from each model
+
+    # concatenate merge output from each model
     ensemble_outputs = [model.output for model in input_models]
     merge = concatenate(ensemble_outputs)
 
-    hidden1 = Dense(DENSE1_OUTPUT_SIZE, activation='relu')(merge)
+    hidden1 = Dense(c.DENSE1_OUTPUT_SIZE, activation="relu")(merge)
     dropout1 = Dropout(0.2)(hidden1)
     batch_norm1 = BatchNormalization()(dropout1)
-    hidden2 = Dense(DENSE2_OUTPUT_SIZE, activation='relu')(batch_norm1)
+    hidden2 = Dense(c.DENSE2_OUTPUT_SIZE, activation="relu")(batch_norm1)
     dropout2 = Dropout(0.2)(hidden2)
     batch_norm2 = BatchNormalization()(dropout2)
-    output = Dense(DENSE3_OUTPUT_SIZE, activation='sigmoid')(batch_norm2)
+    output = Dense(c.DENSE3_OUTPUT_SIZE, activation="sigmoid")(batch_norm2)
     model = Model(inputs=ensemble_inputs, outputs=output)
 
     model.compile(loss="mse", optimizer="adam")
 
-
     return model
 
 
-def fit_overall_model(model, price_data, covid_data, epochs):
-    covid_input = covid_data['x_train_overall']
-    price_input = price_data['x_train_overall']
-    covid_label = covid_data['y_train_overall']
-    price_label = price_data['y_train_overall']
-    # ipdb.set_trace()
-# fit model
-    model.fit([covid_input, price_input], covid_label,
-              epochs=epochs, shuffle=True)
+def fit_overall_model(model, price_data, covid_data):
+    covid_input = covid_data["x_train_overall"]
+    price_input = price_data["x_train_overall"]
+    covid_label = covid_data["y_train_overall"]
+
+    # fit model
+    model.fit(
+        [covid_input, price_input],
+        covid_label,
+        epochs=c.OVERALL_EPOCH,
+        shuffle=c.TRAIN_SHUFFLE,
+    )
+
 
 # make a prediction with a stacked model
-
-
-def predict_overall_model(model, price_data, covid_data,):
-        # prepare input data
-    covid_input = covid_data['x_test']
-    price_input = price_data['x_test']
-# make prediction
-    return model.predict([covid_input, price_input], verbose=0)
+def predict_overall_model(model, price_data, covid_data):
+    # prepare input data
+    covid_input = covid_data["x_test"]
+    price_input = price_data["x_test"]
+    # make prediction
+    return model.predict([covid_input, price_input], verbose=2)
 
 
 def model_prediction(symbol):
     prediction = []
+    scaler = None
+    for i in range(0, c.NUM_TRAIN):
+        price_data, covid_data = prepare_data(
+            c.SAVED_CSV_PATH.format(symbol), shuffle=c.SAMPLE_SHUFFLE
+        )
+        scaler = covid_data["scaler"]
+        print(f"Executing for the {i}th time")
+        model = build_overall_model(price_data, covid_data, batch_size=10)
+        fit_overall_model(model, price_data, covid_data)
 
-    price_data, covid_data = prepare_data(
-        f"{symbol}_daily.csv", shuffle=False)
+        price_data, covid_data = prepare_data(
+            c.SAVED_CSV_PATH.format(symbol), shuffle=False
+        )
 
-    model = build_overall_model(
-        price_data, covid_data, batch_size=10, epochs=200)
-    fit_overall_model(model, price_data, covid_data, epochs=200)
+        print(f"Making {i}th Prediction")
 
-    prediction = predict_overall_model(model, price_data, covid_data)
-    actual_full = np.append(
-        price_data['y_train_individual'], price_data['y_train_overall'])
-    actual_full = np.append(actual_full, price_data['y_test'])
-    prediction_full = np.append(
-        covid_data['y_train_individual'], covid_data['y_train_overall'])
-    prediction_full = np.append(prediction_full, prediction)
-
-    ipdb.set_trace()
-    prediction = covid_data['scaler'].inverse_transform(
-        prediction_full.reshape(-1, 1))
-    actual = price_data['scaler'].inverse_transform(actual_full.reshape(-1, 1))
-    dates = pd.read_csv(f"{symbol}_daily.csv", header=0)['date'].values
-    dates = dates[TIME_STEP:, ]
-    dates = dates[:-PREDICTION_STEP, ]
-    pyplot.plot(dates, prediction, label='prediction')
-    pyplot.plot(dates, actual, label='actual')    
+        if i == 0:
+            prediction = predict_overall_model(model, price_data, covid_data)
+        else:
+            prediction += predict_overall_model(model, price_data, covid_data)
+    prediction = np.array(prediction / c.NUM_TRAIN)
+    label = np.array(covid_data["y_test"][:, 2]).reshape(-1, 1)
+    pyplot.plot(
+        scaler.inverse_transform(prediction[:, 0].reshape(-1, 1)),
+        label="prediction_first",
+    )
+    pyplot.plot(
+        scaler.inverse_transform(prediction[:, c.PREDICTION_STEP - 1].reshape(-1, 1)),
+        label="prediction_last",
+    )
+    pyplot.plot(scaler.inverse_transform(label), label="actual")
     pyplot.legend()
     pyplot.show()
 
@@ -265,6 +197,4 @@ if __name__ == "__main__":
     if args.symbol:
         model_prediction(args.symbol)
         exit(1)
-    raise ValueError(
-        "Missing symbol"
-    )
+    raise ValueError("Missing symbol")
